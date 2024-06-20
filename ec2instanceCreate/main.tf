932b4ec3f0708c4d7aff03e1cae98e46c58dec42
allthingsross <all.things.ross@proton.me>
2024-06-20 13:23:20 +0100

    Corrected syntax for SGs - a list of one string not a single value

--- ec2instanceCreate/main.tf
+++ ec2instanceCreate/main.tf
@@ -15,10 +15,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "exampleServer" {
-  ami           = "ami-07d1e0a32156d0d21"
-  instance_type = "t2.micro"
-  key_name	= "aws"
-  security_groups	= "launch-wizard-2"
+  ami             = "ami-07d1e0a32156d0d21"
+  instance_type   = "t2.micro"
+  key_name        = "aws"
+  security_groups = ["launch-wizard-2"]
 
   tags = {
     Name = "exampleServer"
