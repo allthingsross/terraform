dd8c1a5f2e9b04e3fa96a1a6f08c7dec8d0c18c6
allthingsross <all.things.ross@proton.me>
2024-06-20 12:23:11 +0100

    Example EC2 create terraform script

---
+++ ec2instanceCreate/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+  profile = "terrafrom"
+}
+
+resource "aws_instance" "exampleServer" {
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "exampleServer"
+  }
+}
